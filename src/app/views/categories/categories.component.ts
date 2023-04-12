9340274f215b5b55d23fc239a53f5cb9b089e93e
Admin <1q.expert@gmail.com>
2023-04-12 12:25:51 +0300

    added view category in left menu - sidebar

---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit{
+  categories: Category[] = []
+  constructor(private dataHendler: DataHandlerService) {
+
+  }
+  ngOnInit(): void {
+   this.categories = this.dataHendler.getCategories()
+  }
+
+}
